f06217a2bc64b6a00921961cdf96f81434f49b3a
ShubhamChouhan97 <shubhamsc9799@gmail.com>
2025-03-18 17:24:10 +0530

    server url set

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  preview: {
+    host: '0.0.0.0',
+    port: process.env.PORT || 4173, // Use Render's port or fallback to 3000
+  },
+  server: {
+    host: '0.0.0.0',
+    port: process.env.PORT || 3000,
+  },
+});
